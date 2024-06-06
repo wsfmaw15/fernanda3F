d8ed742cf003dfc3fbcc129144e0ac5db54e04d6
wsfmaw15 <127457500+wsfmaw15@users.noreply.github.com>
2024-06-05 22:30:50 -0300

    Update main.js
    0506

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = Document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+     botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo")
+        }
+
+        botoes[i].classList.add("ativo");
+    
+    }
+}
